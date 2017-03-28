3225197daa33e82adf1a5e14b2acd97fe1c752c0
new_user_name <new@email.com>
2017-03-28 11:07:36 +0600

    Initial Commit

---
+++ YandexTranslator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YandexTranslator
+//
+//  Created by Admin on 3/28/17.
+//  Copyright © 2017 AAkash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
